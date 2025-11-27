b7a50296772a8b2915939913444bbce7e7353cf2
MihajloMilojevic <milojevicm374@gmail.com>
2025-11-27 13:44:52 +0100

    stage 11, cd home

--- app/commands/builtins/CdCmd.go
+++ app/commands/builtins/CdCmd.go
@@ -19,6 +19,12 @@ func (c *CdCommand) Execute(IN, OUT *os.File) (bool, error) {
 	if len(c.args) < 1 {
 		return false, nil // No directory provided; do nothing
 	}
+	if c.args[0] == "~" {
+		homeDir := os.Getenv("HOME")
+		if homeDir != "" {
+			c.args[0] = homeDir
+		}
+	}
 	err := os.Chdir(c.args[0])
 	if err != nil {
 		OUT.WriteString("cd: " + c.args[0] + ": No such file or directory\n")
