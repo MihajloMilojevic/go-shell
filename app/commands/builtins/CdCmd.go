927cf8a0e6605de507ee8ce96dc7705a900416de
MihajloMilojevic <milojevicm374@gmail.com>
2025-11-27 13:42:29 +0100

    stage 10, cd absolute paths

---
+++ app/commands/builtins/CdCmd.go
@@ -0,0 +1,31 @@
+package builtins
+
+import (
+	"os"
+
+	"github.com/codecrafters-io/shell-starter-go/app/commands"
+	"github.com/codecrafters-io/shell-starter-go/app/commands/registries"
+)
+
+type CdCommand struct {
+	args []string
+}
+
+func NewCdCommand(cmd string, args []string) commands.Command {
+	return &CdCommand{args: args}
+}
+
+func (c *CdCommand) Execute(IN, OUT *os.File) (bool, error) {
+	if len(c.args) < 1 {
+		return false, nil // No directory provided; do nothing
+	}
+	err := os.Chdir(c.args[0])
+	if err != nil {
+		OUT.WriteString("cd: " + c.args[0] + ": No such file or directory\n")
+	}
+	return false, nil
+}
+
+func init() {
+	registries.RegisterBuiltin("cd", NewCdCommand)
+}
